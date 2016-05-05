8d727c85f58d48c22c5cc1b1350b3f01fae4dbe0
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-05-05 16:48:44 +0100

    react gem added

--- config/initializers/jsroutes.rb
+++ config/initializers/jsroutes.rb
@@ -1,4 +1,8 @@
 JsRoutes.setup do |config|
+  # Whitelist routes to include on the Front-End
+  # NOTE: if you add a new route here, do not forget to run:
+  #       $ rake tmp:cache:clear
+  #       before restarting your `rails s`.
   config.include = [
     /^posts_sub_categories$/,
     /^upvote_post$/,
