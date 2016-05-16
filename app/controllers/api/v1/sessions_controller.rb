b7285fa281ed705954ba39269da81b304c58943f
Matthias Richard <matthias.rms@gmail.com>
2016-05-16 23:20:46 +0200

    api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < Api::V1::BaseController
 
- acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+ #acts_as_token_authentication_handler_for User, except: [ :index, :show ]
  before_action :set_session, only: [ :show, :update ]
 
  def update
@@ -12,13 +12,12 @@ class SessionsController < ApplicationController
  end
 
  def create
-   @session = current_user.sessions.build(session_params)
-   authorize @session
-   if @session.save
-     render :show
-   else
-     render_error
-   end
+   @session = find_or_create_session(params[:fbid])
+   p @session
+   @room = find_or_create_room(sender_id, sender)
+   @message = Message.new({content: msg, room_id: @room.id, sender: "user"})
+   @message.save!
+
  end
 
  private
@@ -30,5 +29,15 @@ class SessionsController < ApplicationController
  def render_error
    render json: { errors: @session.errors.full_messages }, status: :unprocessable_entity
  end
+
+ def find_or_create_session(fbid, max_age: 2.minutes)
+    Session.find_by(["facebook_id = ? AND last_exchange >= ?", fbid, max_age.ago]) ||
+    Session.create(facebook_id: fbid, context: {})
+  end
+
+  def find_or_create_room(fbid, sender)
+    Room.find_by(["facebook_id = ?", fbid]) ||
+    Room.create(facebook_id: fbid, first_name: sender.get_profile[:body]["first_name"])
+  end
 
 end
