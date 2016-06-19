88f9fbcb7ac24836f47d6f934d01d66d4f7d57a0
Matthias Richard <matthias.rms@gmail.com>
2016-06-19 16:12:11 +0200

    fix

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -21,20 +21,17 @@ class MessageBroadcastJob < ApplicationJob
   def render_message(message)
     ApplicationController.renderer.render(partial: 'messages/text_message', locals: { message: message })
   end
-   def render_bot_message(message)
-    # p message
-    # p "CLASS"
-    # p message.content.class
-    # p eval(message.content).class
-      if message.structured_messages.nil? == false && message.structured_messages["attachment"].present?
-        if (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "generic"
-          ApplicationController.renderer.render(partial: "messages/structured_message_carousel", locals: { message: (eval(message.structured_messages).with_indifferent_access)})
-        elsif (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "button"
-          ApplicationController.renderer.render(partial: "messages/structured_message_button", locals: { message: (eval(message.structured_messages).with_indifferent_access)})
-        elsif (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "receipt"
-          ApplicationController.renderer.render(partial: "messages/structured_message_receipt",locals: { message: (eval(message.structured_messages).with_indifferent_access)})
-        else
-          ApplicationController.renderer.render(partial: "messages/image",locals: { message: message })
+
+  def render_bot_message(message)
+    if message.structured_messages.nil? == false && message.structured_messages["attachment"].present?
+      if (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "generic"
+        ApplicationController.renderer.render(partial: "messages/structured_message_carousel", locals: { message: (eval(message.structured_messages).with_indifferent_access)})
+      elsif (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "button"
+        ApplicationController.renderer.render(partial: "messages/structured_message_button", locals: { message: (eval(message.structured_messages).with_indifferent_access)})
+      elsif (eval(message.structured_messages).with_indifferent_access)[:attachment][:payload][:template_type] == "receipt"
+        ApplicationController.renderer.render(partial: "messages/structured_message_receipt",locals: { message: (eval(message.structured_messages).with_indifferent_access)})
+      else
+        ApplicationController.renderer.render(partial: "messages/image",locals: { message: message })
       end
     else
       ApplicationController.renderer.render(partial: "messages/text_message", locals: { message: message })
